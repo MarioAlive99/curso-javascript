0bf1412833f6087e001614467aa027afb5e5667f
MarioDevs <107438509+MarioAlive99@users.noreply.github.com>
2024-08-15 23:43:24 -0600

    Add files via upload

---
+++ LECCION 1. VARIABLES/01.js
@@ -0,0 +1,26 @@
+// Declarar e inicializar una variable con var
+var edad = 30;
+
+console.log(edad);
+
+// Error de una variable var
+//var nombre;
+//console.log(nombre);
+
+// Declarar e inicializar una variable con let
+let edad2 = 3;
+
+console.log(edad2);
+
+// Error de variable let
+//let nombre2;
+//console.log(nombre2);
+
+// Declarar e inicializar una variable con const.
+const PI = 3.1415;
+
+console.log(PI);
+
+//Error de variable const
+//const EULER = 3.4563;
+//EULER = 3.45;
